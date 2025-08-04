3336cc7aabba96a5165df0d9be37f2bfff7f3ad7
2DevGs <dev.gustavoserrano@gmail.com>
2025-08-04 20:22:27 -0300

    Concepts of internationalization
    Working with translation keys via enum.

--- lib/pages/core/translation/translation_keys.dart
+++ lib/pages/core/translation/translation_keys.dart
@@ -1,10 +1,53 @@
 import 'package:get/get.dart';
 
+enum TranslateKey {
+  titleApp('title_app'),
+  welcome('welcome'),
+  userListTitleSimple(
+    'user_list_title_simple',
+    plural: 'user_list_title_simple_plural',
+  ),
+  userListTitle(
+    'user_list_title',
+    plural: 'user_list_title_plural',
+  );
+
+  final String key;
+  final String? plural;
+
+  const TranslateKey(this.key, {this.plural});
+
+  String tr() {
+    return key.tr;
+  }
+  
+  String get trr => key.tr;
+
+  String trParams(Map<String, String> params) {
+    return key.trParams(params);
+  }
+
+  String trPlural(int total) {
+    if(plural != null){
+      return key.trPlural(plural, total);
+    }
+    return '';
+  }
+
+  String trPluralParams(int total, Map<String, String> params) {
+    if(plural != null){
+      return key.trPluralParams(plural, total, params);
+    }
+    return '';
+  }
+
+}
+
 class TranslationKeys extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
         'en_US': {
-          'title_app': 'GetX Translate',
+          TranslateKey.titleApp.key: 'GetX Translate',
           'welcome': 'Hello @name',
           'user_list_title_simple': 'User',
           'user_list_title_simple_plural': 'Users',
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_translate/pages/core/translation/translation_keys.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,15 +9,24 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('title_app'.tr),
+        title: Text(TranslateKey.titleApp.trr),
+        // title: Text(TranslateKey.titleApp.tr()),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('welcome'.trParams({'name': 'Gustavo Dias'})),
+            Text(TranslateKey.welcome.trParams({'name': 'Gustavo Dias'})),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text('trPlural'),
             Text('user_list_title_simple'.trPlural('user_list_title_simple_plural', 1)),
             Text('user_list_title_simple'.trPlural('user_list_title_simple_plural', 2)),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text('trPluralParams'),
             Text('user_list_title'.trPluralParams(
               'user_list_title_plural',
               2,
@@ -46,6 +56,28 @@ class HomePage extends StatelessWidget {
               },
               child: const Text('Alterar BR'),
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text('Enum trPlural'),
+            Text(TranslateKey.userListTitleSimple.trPlural(1)),
+            Text(TranslateKey.userListTitleSimple.trPlural(2)),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text('Enum trPluralParams'),
+            Text(TranslateKey.userListTitle.trPluralParams(
+              2,
+              {
+                'total': '2',
+              },
+            )),
+            Text(TranslateKey.userListTitle.trPluralParams(
+              1,
+              {
+                'total': '1',
+              },
+            )),
           ],
         ),
       ),
